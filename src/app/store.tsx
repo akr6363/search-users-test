616bf89ff018cee06102501ab3da0ab44bc179ef
Anna Krasilnikova <akr6363@mail.ru>
2023-07-28 00:26:00 +0300

    create store

---
+++ src/app/store.tsx
@@ -0,0 +1,21 @@
+import { AnyAction, combineReducers } from "redux";
+
+import { configureStore } from "@reduxjs/toolkit";
+import thunkMiddleware, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import {usersReducer} from "../model/users/users-slice.ts";
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware),
+});
+
+export type AppRootStateType = ReturnType<typeof store.getState>;
+export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>;
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>;
+// @ts-ignore
+window.store = store;
