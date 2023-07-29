b2b0a0aa4142167fd2a4c54352169ce682373e22
Anna Krasilnikova <akr6363@mail.ru>
2023-07-29 14:59:08 +0300

    add test for users-slice

--- src/model/users/users-slice.test.ts
+++ src/model/users/users-slice.test.ts
@@ -1,73 +1,45 @@
-// import {
-//   InitialStateType,
-//   postsActions,
-//   postsReducer,
-//   postsThunks,
-//   RequestStatusType,
-//   SortDirectionType,
-//   SortKeyType,
-// } from "../posts-reducer";
-// import { PostType } from "components/Posts/posts-api";
-// import { beforeEach } from 'jest';
-// let initialState: InitialStateType;
-//
-// beforeEach(() => {
-//   initialState = {
-//     posts: [],
-//     currentPage: 1,
-//     pageSize: 10,
-//     pageTotalCount: 0,
-//     searchValue: "",
-//     sortParams: {
-//       sortDirection: "asc" as SortDirectionType,
-//       sortKey: "id" as SortKeyType,
-//     },
-//     status: "idle" as RequestStatusType,
-//     error: null,
-//   };
-// });
-//
-// test("posts should be set", () => {
-//   const posts: PostType[] = [
-//     { userId: 1, id: 1, title: "Post 1", body: "Body of post 1" },
-//     { userId: 2, id: 2, title: "Post 2", body: "Body of post 2" },
-//     { userId: 3, id: 3, title: "Post 3", body: "Body of post 3" },
-//   ];
-//   const action = postsThunks.fetchPosts.fulfilled({ posts }, "requestId");
-//   const newState = postsReducer(initialState, action);
-//   expect(newState.posts).toEqual(posts);
-// });
-//
-// test("page total count should be set", () => {
-//   const newState = postsReducer(initialState, postsActions.setPageTotalCount({ pageTotalCount: 10 }));
-//   expect(newState.pageTotalCount).toEqual(10);
-// });
-// test("current page should be set", () => {
-//   const newState = postsReducer(initialState, postsActions.setCurrentPage({ currentPage: 2 }));
-//   expect(newState.currentPage).toEqual(2);
-// });
-//
-// test("search value should be set", () => {
-//   const newState = postsReducer(initialState, postsActions.setSearchValue({ value: "abc" }));
-//   expect(newState.searchValue).toEqual("abc");
-// });
-//
-// test("sort params should be set", () => {
-//   const newParams = {
-//     sortDirection: "desc" as SortDirectionType,
-//     sortKey: "title" as SortKeyType,
-//   };
-//   const newState = postsReducer(initialState, postsActions.setSortParams({ params: newParams }));
-//   expect(newState.sortParams).toEqual(newParams);
-// });
-//
-// test("status should be set", () => {
-//   const newState = postsReducer(initialState, postsActions.setStatus({ status: "loading" }));
-//   expect(newState.status).toEqual("loading");
-// });
-//
-// test("error should be set", () => {
-//   const newState = postsReducer(initialState, postsActions.setError({ error: "some error" }));
-//   expect(newState.error).toEqual("some error");
-// });
-// export {};
+import {InitialStateType, SortDirectionType, SortKeyType, usersActions, usersReducer} from "./users-slice.ts";
+import {UserInfoType} from "./users-api.ts";
+
+let initialState: InitialStateType;
+beforeEach(() => {
+    initialState = {
+        users: [],
+        userInfo: {} as UserInfoType,
+        totalCount: 0, //всего найденных пользователей
+
+        searchParams: {
+            searchValue: '',
+            page: 1, //текущая страница
+            pageSize: 30, //кол-во отображаемых пользователей на одной странице
+        },
+        sortParams: {
+            sortKey: null,
+            sortDirection: null,
+        },
+    }
+});
+
+test("total count should be set", () => {
+    const newState = usersReducer(initialState, usersActions.setTotalCount({totalCount: 10}));
+    expect(newState.totalCount).toEqual(10);
+});
+
+test("sort params should be set", () => {
+    const newParams = {
+        sortDirection: "desc" as SortDirectionType,
+        sortKey: "repositories" as SortKeyType,
+    };
+    const newState = usersReducer(initialState, usersActions.setSortParams({params: newParams}));
+    expect(newState.sortParams).toEqual(newParams);
+});
+
+test("searchParams params should be set", () => {
+    const newParams = {
+        searchValue: 'value',
+        page: 5,
+        pageSize: 50,
+    };
+    const newState = usersReducer(initialState, usersActions.setSearchParams({params: newParams}));
+    expect(newState.searchParams).toEqual(newParams);
+});
