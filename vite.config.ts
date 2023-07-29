fe78ea94c857f46c268e7b55ef3a56cfe0470e09
Anna Krasilnikova <akr6363@mail.ru>
2023-07-29 16:57:27 +0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/search-users-test/',
+  base: 'https://akr6363.github.io/search-users-test',
   plugins: [react()],
 })
